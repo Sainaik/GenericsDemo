a793646e54efbad0d1b02664b724678bc12670a5
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-20 22:03:17 +0530

    [Sai]Refactor. Used Generic class to get max

--- GenericsDemo/NewProgram.cs
+++ GenericsDemo/NewProgram.cs
@@ -9,37 +9,51 @@ namespace GenericsDemo
     public class NewProgram
     {
 
-        public T FindMax<T>(T input1, T input2, T input3)
-        {
-            T[] inputArray = { input1, input2, input3 };
-
-            Array.Sort(inputArray);
-
-            var max = inputArray[inputArray.Length - 1];
-
-
-            Console.WriteLine($"Max number is : { max}");
-            return max;
-
-        }
-
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics Demo Project!!");
 
-            NewProgram maxOfIntegers = new NewProgram();
+            
+            GenericMaxClass<int> intMax = new GenericMaxClass<int>(10, 200, 200);
+            intMax.getMaximum();
 
-            maxOfIntegers.FindMax<int>(10, 200, 23);
 
-            NewProgram maxOfDoubles = new NewProgram();
 
-            maxOfDoubles.FindMax<double>(10.1, 20.2, 20.1);
+            GenericMaxClass<double> doubleMax = new GenericMaxClass<double>(10.1, 20.2, 20.1);
+            doubleMax.getMaximum();
 
-            NewProgram maxOfStrings = new NewProgram();
+            
+            GenericMaxClass<string> stringMax = new GenericMaxClass<string>("abcd", "143", "ABCD");
+            stringMax.getMaximum();
 
-            maxOfStrings.FindMax<string>("abcd", "143", "ABCD");
 
         }
 
+       
     }
+
+    public class GenericMaxClass<T> where T : IComparable
+    {
+        public T input1,  input2,  input3;
+        public GenericMaxClass(T input1, T input2, T input3 )
+        {
+            this.input1 = input1;
+            this.input2 = input2;
+            this.input3 = input3;
+        }
+        public T getMaximum()
+        {
+
+
+            var max = this.input1.CompareTo(input2) > 0 ? input1 : (this.input2.CompareTo(input3) > 0 ? input2:input3) ;
+
+            //this.array[this.array.Length - 1];
+
+            Console.WriteLine("Max is :" + max);
+
+            return max;
+        }
+    }
+
 }
--- GenricsMaxTest/UnitTest1.cs
+++ GenricsMaxTest/UnitTest1.cs
@@ -11,11 +11,9 @@ namespace GenricsMaxTest
         [DataRow(13,99,12,99)]
         [DataRow(100,1000,10000,10000)]
         public void FindMax_GivenIntegers_MatchExpcted(int num1, int num2, int num3,int expected)
-        {
-            NewProgram maxOfIntegers = new NewProgram();
-
-            int actual = maxOfIntegers.FindMax<int>(num1,num2,num3);
-
+        {        
+            GenericMaxClass<int> intMax = new GenericMaxClass<int>(num1,num2,num3);
+            int actual = intMax.getMaximum();
             Assert.AreEqual(expected, actual);
         }
 
@@ -25,10 +23,8 @@ namespace GenricsMaxTest
         [DataRow(10.0, 10.10, 100.00, 100.00)]
         public void FindMax_Given3Float_MatchExpected(double num1, double num2, double num3, double expected)
         {
-            NewProgram maxOfDouble = new NewProgram();
-
-            double actual = maxOfDouble.FindMax<double>(num1, num2, num3);
-
+            GenericMaxClass<double> doubleMax = new GenericMaxClass<double>(num1, num2, num3);
+            double actual = doubleMax.getMaximum();
             Assert.AreEqual(expected, actual);
         }
 
@@ -39,9 +35,9 @@ namespace GenricsMaxTest
         [DataRow("xyz","XYZa","SAI","XYZa")]
         public void FindMax_Given3Strings_MatchExpected(string str1, string str2, string str3, string expected)
         {
-            NewProgram maxOfStrings = new NewProgram();
-
-            string actual = maxOfStrings.FindMax<string>(str1, str2, str3);
+            GenericMaxClass<string> stringMax = new GenericMaxClass<string>(str1, str2, str3);
+            
+            string actual = stringMax.getMaximum();
 
             Assert.AreEqual(expected, actual);
         }
