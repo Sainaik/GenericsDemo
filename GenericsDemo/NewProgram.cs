6e9c4792167f0abbec29de67b18e6f6373ed77eb
Sai Kumar Naik <saikumar.naik7777@gmail.com>
2020-10-20 16:36:58 +0530

    [Sai]First Commit

---
+++ GenericsDemo/NewProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class NewProgram
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics Demo Project!!");
+        
+        }
+    }
+}
